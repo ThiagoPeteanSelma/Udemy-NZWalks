3ed943a3ecc053603599464927316229bdaeae28
ThiagoPeteanSelma <159153124+ThiagoPeteanSelma@users.noreply.github.com>
2024-02-16 14:56:11 -0300

    Section 7

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -35,12 +35,14 @@ namespace NZWalks.API.Controllers
         }
 
         // GET ALL WALKS
-        // GET: http://localhost:portnumber/api/walks
+        // GET: http://localhost:portnumber/api/walks?filterOn=Name&fiterQuery=Track&sortby=Name&isAscending=true&pageNumber=1&pageSize=10
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery, 
+            [FromQuery] string? sortBy, [FromQuery] bool? isAscending,
+            [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 1000)
         {
             // Get Data From Database - Domain models
-            var walkDomainModel = await walkRepository.GetAllAsync();
+            var walkDomainModel = await walkRepository.GetAllAsync(filterOn, filterQuery, sortBy, isAscending ?? true);
 
             // Return DTOs
             return Ok(mapper.Map<List<WalkDTO>>(walkDomainModel));
--- NZWalks.API/Repositories/SQLWalkRepository.cs
+++ NZWalks.API/Repositories/SQLWalkRepository.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
 using NZWalks.API.Models.Domain;
+using StructureMap.Pipeline;
 
 namespace NZWalks.API.Repositories
 {
@@ -34,9 +36,53 @@ namespace NZWalks.API.Repositories
             return existingWalk;
         }
 
-        public async Task<List<Walk>> GetAllAsync()
+        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null, 
+            string? sortBy = null, bool isAscending = true, int pageNumber = 1, int pageSize = 1000)
         {
-            return await dbContext.Walks.Include("Difficulty").Include("Region").ToListAsync();
+            var walks = dbContext.Walks.Include("Difficulty").Include("Region").AsQueryable();
+
+            // Filtering
+    
+            if(!string.IsNullOrWhiteSpace(filterOn) && !string.IsNullOrWhiteSpace(filterQuery)) 
+            {
+                switch (filterOn.ToString().ToLower())
+                {
+                    case "name":
+                        walks = walks.Where(x=> x.Name == filterQuery);
+                        break;
+                    case "description":
+                        walks = walks.Where(x => x.Description == filterQuery);
+                        break;
+                    default:
+                        throw new ArgumentException("Filter not implemented");
+                }
+            }
+
+            // Sorting
+            if (!string.IsNullOrWhiteSpace(sortBy))
+            {
+                switch (sortBy.ToString().ToLower())
+                {
+                    case "name":
+                        walks = isAscending? walks.OrderBy(x=> x.Name) : walks.OrderByDescending(x => x.Name);
+                        break;
+                    case "description":
+                        walks = isAscending ? walks.OrderBy(x => x.Description) : walks.OrderByDescending(x => x.Description);
+                        break;
+                    case "length":
+                        walks = isAscending ? walks.OrderBy(x => x.LengthInKm) : walks.OrderByDescending(x => x.LengthInKm);
+                        break;
+                    default:
+                        throw new ArgumentException("Sort not implemented");
+                }
+            }
+
+            // Pagination
+
+            var skipresults = (pageNumber - 1) * pageSize;
+
+
+            return await walks.Skip(skipresults).Take(pageSize).ToListAsync();
         }
 
         public async Task<Walk?> GetByIdAsync(Guid id)
